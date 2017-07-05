0fd91dc197b73e61b0b7275a2486426d8cec1ce4
SamitoDeveloper <luizsamuel_5@hotmail.com>
2017-07-05 18:26:15 -0300

    fyrm
    05/07/2017

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'page-login',
@@ -8,9 +10,32 @@ import { NavController } from 'ionic-angular';
 
 export class LoginPage {
   
-  titulo = 'For You To Remember Me';
-  constructor() {
-    
-  }
+	titulo = 'For You To Remember Me';
+
+	url = 'http://br400.teste.website/~appot240/for_you_to_remember_me/';
+	loader;
+	constructor(public loadingCtrl: LoadingController, public http: Http) {
+
+	}
+
+	LogarUsuario(){
+		this.InicializarLoading();
+
+	}
+
+	VerificaLoginUsuario(){
+		
+	}
+
+	InicializarLoading() { 
+		this.loader = this.loadingCtrl.create({
+			content: "Carregando..."
+		}); 
+		this.loader.present()
+	}
+
+	EncerraLoading(){
+		this.loader.dismiss();
+	}
 
 }
